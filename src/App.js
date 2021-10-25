1a661eb250e0cf32a619246ec17996ecf201fa0f
EdenReuven <74300459+EdenReuven@users.noreply.github.com>
2021-10-26 01:55:56 +0300

    style about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import react from "react";
+import GlobalStyle from "./components/GlobalStyled";
 import AboutUs from "./pages/AboutUs"
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle/>
       <AboutUs/>
     </div>
   );
