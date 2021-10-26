057c66d6f799de561802b94a185e4717f9113936
EdenReuven <74300459+EdenReuven@users.noreply.github.com>
2021-10-26 03:21:47 +0300

    create Nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import react from "react";
 import GlobalStyle from "./components/GlobalStyled";
 import AboutUs from "./pages/AboutUs"
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
+      <Nav/>
       <AboutUs/>
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,52 @@
+import react from "react";
+import styled from "styled-components";
+
+const Nav =() =>{
+    return(
+        <StyledNav>
+            <h1>
+                <a id="logo" href="#">Capture</a>
+            </h1>
+            <ul>
+                <li>
+                    <a href="#"> 1.about us </a>
+                </li>
+                <li>
+                    <a href="#"> 2.Our work </a>
+                </li>
+                <li>
+                    <a href="#"> 3.Contact us </a>
+                </li>
+            </ul>
+        </StyledNav>
+    );
+};
+
+const StyledNav =styled.nav`
+    min-height: 10vh;
+    display: flex;
+    margin: auto;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 10rem;
+    background: #282828;
+    a{
+        color: white;
+        text-decoration: none;
+    }
+    ul{
+        display: flex;
+        list-style: none;
+    }
+    #logo{
+        font-size: 1.5rem;
+        font-weight: lighter;
+        font-family: "Lobster" ,cursive;
+    }
+    li{
+        padding-left: 10rem;
+        position: relative;
+    }
+` 
+
+export default Nav;
