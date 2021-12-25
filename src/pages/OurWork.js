01d5611df4e2eb80e740dfac4e57df4171d153c6
EdenReuven <74300459+EdenReuven@users.noreply.github.com>
2021-12-25 05:54:21 +0200

    our work scroll

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -15,8 +15,11 @@ import {
   lineAnim,
   slider,
 } from "../animation";
+import {UseScroll} from "../components/useScroll";
 
 const OurWork = () => {
+  const [element , controls] = UseScroll();
+  const [element2 , controls2] = UseScroll();
   return (
     <Work
       style={{ background: "#fff" }}
@@ -41,16 +44,16 @@ const OurWork = () => {
           </Hide>
         </Link>
       </Movie>
-      <Movie>
+      <Movie ref={element} variants={fade} initial="hidden" animate={controls}>
         <h2>The Racer</h2>
-        <div className="line"></div>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-racer">
           <img src={theracer} alt="theracer" />
         </Link>
       </Movie>
-      <Movie>
+      <Movie ref={element2} variants={fade} initial="hidden" animate={controls2}>
         <h2>Good Times</h2>
-        <div className="line"></div>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/good-times">
           <img src={goodtimes} alt="goodtimes" />
         </Link>
@@ -67,7 +70,7 @@ const Work = styled(motion.div)`
     padding: 1rem 0rem;
   }
 `;
-const Movie = styled.div`
+const Movie = styled(motion.div)`
   padding-bottom: 10rem;
   .line {
     height: 0.5rem;
